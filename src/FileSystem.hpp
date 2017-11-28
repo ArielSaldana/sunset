e76a93b5161562d8004adf7bda8f4bf4ecb9d8eb
Ariel Saldana <hello@ariel.io>
2017-11-28 00:10:40 -0500

    FS

---
+++ src/FileSystem.hpp
@@ -0,0 +1,14 @@
+#ifndef FILE_SYSTEM_HPP
+#define FILE_SYSTEM_HPP
+
+#pragma once
+
+#include <string>
+
+class FileSystem {
+public:
+    readFile(std::string fileName);
+    readDirectory(std::string);
+};
+
+#endif//FILE_SYSTEM_HPP
